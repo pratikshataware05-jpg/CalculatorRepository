4069446e7c28b0cf90e2d775b655147abcd7bc01
pratikshataware <96300978+pratikshataware@users.noreply.github.com>
2025-09-10 15:05:52 +0530

    add testtt class

---
+++ Calculator1/src/com/app/Testt.java
@@ -0,0 +1,25 @@
+package com.app;
+
+public class Testt {
+	
+	public void add() {
+		System.out.println(10+20);
+	}
+	public void sub(){
+System.out.println(20-10);
+	}
+	public void mul(){
+		System.out.println(10*23);
+	}
+	public void addition(){
+System.out.println(100+200);
+	}
+	
+	public static void main(String[] args) {
+		
+		Testt t = new Testt ();
+		t.add();
+		
+	}
+
+}
