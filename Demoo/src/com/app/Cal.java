7fbc511ce185d4122d4ec49dbc8a934ab7be3867
pratikshataware <pratikshataware05@gmail.com>
2025-09-10 15:21:27 +0530

    Add add,sub,mul method

--- Demoo/src/com/app/Cal.java
+++ Demoo/src/com/app/Cal.java
@@ -7,9 +7,16 @@ System.out.println(10+30);
 public void sub() {
 	System.out.println(20-10);
 }
+	public void mul(){
+		System.out.println(12*10);
+	}
 	
 	public static void main(String[] args) {
-		
+
+		Cal c = new Cal();
+		c.add();
+		c.sub();
+		s.mul();
 	}
 
 }
