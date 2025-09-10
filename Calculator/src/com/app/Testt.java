531a61cb507e6dc70c5dd001da897b591633b979
pratikshataware <pratikshataware05@gmail.com>
2025-09-10 14:31:51 +0530

    Add addition method

--- Calculator/src/com/app/Testt.java
+++ Calculator/src/com/app/Testt.java
@@ -11,6 +11,9 @@ System.out.println(20-10);
 	public void mul(){
 		System.out.println(10*23);
 	}
+	public void addition(){
+System.out.println(100+200);
+	}
 	
 	public static void main(String[] args) {
 		
