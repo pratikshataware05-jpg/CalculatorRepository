3ee7b1970dc455fee499b369ef53fa6dfad5e5e9
pratikshataware <96300978+pratikshataware@users.noreply.github.com>
2025-09-10 13:57:08 +0530

    add method from Dev1

--- Calculator/src/com/app/Testt.java
+++ Calculator/src/com/app/Testt.java
@@ -1,9 +1,15 @@
 package com.app;
 
 public class Testt {
+	
+	public void add() {
+		System.out.println(10+20);
+	}
+	
 	public static void main(String[] args) {
 		
-		
+		Testt t = new Testt ();
+		t.add();
 		
 	}
 
